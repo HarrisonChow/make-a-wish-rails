b20877c606c0f043fa0d7bd72167c92052a3c4c7
Airwoman <airwomanzhy@hotmail.com>
2016-08-21 23:09:45 +1000

    create wish function done

---
+++ app/controllers/wishes_controller.rb
@@ -0,0 +1,27 @@
+class WishesController < ApplicationController
+  def index
+    @wishes = Wish.all
+  end
+
+  def new
+
+  end
+
+  def create
+    @wish = Wish.new(wish_params)
+
+    if @wish.save
+      redirect_to wishes_path
+    else
+      render :new
+    end
+  end
+
+
+
+  private
+
+  def wish_params
+    params.require(:wish).permit(:content)
+  end
+end
